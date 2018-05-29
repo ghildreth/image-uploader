8821115d8d49c575cfee8b330eca238784607f12
ghildreth <gabrielantonhildreth@gmail.com>
2018-05-29 09:38:52 -0700

    edit models create post, and post_attachments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    has_many :post_attachments
+    accepts_nested_attributes_for :post_attachments
+    # Nested attributes allow you to save attributes
+    # on assoicated records through the parent
 end
